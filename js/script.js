e762b35635dc41b088c27e9f4ada55eb7ecf1b4f
qrxt <46269438+qrxt@users.noreply.github.com>
2019-03-22 04:04:27 +0300

    начало скриптов, фикс кнопки выпадающего окна

---
+++ js/script.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const overlay = document.querySelector(".overlay");
+
+const writeUsViewButton = document.querySelector(".contacts-button");
+const writeUsPopup = document.querySelector(".write-us");
+const writeUsForm = document.querySelector(".write-us-form");
+const writeUsFormName = document.querySelector("[name=write-us-name]");
+const writeUsFormEmail = document.querySelector("[name=write-us-email]");
+const writeUsFormMessage = document.querySelector("[name=write-us-message]");
+const writeUsPopupCloseButton = document.querySelector(".write-us-close");
+const writeUsFormFields = [writeUsFormName, writeUsFormEmail, writeUsFormMessage];
+
+const locationViewButton = document.querySelector(".contacts-map");
+const locationPopup = document.querySelector(".our-location");
+
+locationViewButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    overlay.classList.add("show");
+    locationPopup.classList.add("show");
+});
+
+locationViewButton.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) closeLocationPopup();
+});
+
+const dataFromStorage = (localStorage && localStorage.getItem("userName") && localStorage.getItem("userEmail")) ?
+    { name: localStorage.getItem("userName"), email: localStorage.getItem("userEmail") }
+    : null;
+
+const closeWriteUsPopup = function () {
+    overlay.classList.remove("show");
+    writeUsPopup.classList.remove("show");
+}
+
+const closeLocationPopup = function () {
+    overlay.classList.remove("show");
+    locationPopup.classList.remove("show");
+}
+
+writeUsViewButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    overlay.classList.add("show");
+    writeUsPopup.classList.add("show");
+
+    if (dataFromStorage) {
+        writeUsFormName.value = dataFromStorage.name;
+        writeUsFormEmail.value = dataFromStorage.email;
+        writeUsFormMessage.focus();
+    } else writeUsFormName.focus();
+});
+
+writeUsForm.addEventListener("submit", function (event) {
+    if (!writeUsFormName.value || !writeUsFormEmail.value || !writeUsFormMessage.value) {
+        event.preventDefault();
+        writeUsPopup.classList.add("modal-error");
+        setTimeout(function () {
+            writeUsPopup.classList.remove("modal-error");
+        }, 600);
+        writeUsFormFields.forEach(function (item) {
+            if (!item.value) {
+                item.classList.add("incorrect");
+            }
+        })
+    } else if (localStorage) {
+        localStorage.setItem("userName", writeUsFormName.value);
+        localStorage.setItem("userEmail", writeUsFormEmail.value);
+    }
+});
+
+writeUsPopupCloseButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    closeWriteUsPopup();
+});
+
+writeUsPopup.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) closeWriteUsPopup();
+});
+
+overlay.addEventListener("click", function (event) {
+    closeWriteUsPopup();
+    closeLocationPopup();
+});
