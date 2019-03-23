405b9e053fd4e29e4161151c9f93abf78e9025ed
qrxt <qrx357@gmail.com>
2019-03-23 22:47:08 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -13,35 +13,40 @@ const writeUsFormFields = [writeUsFormName, writeUsFormEmail, writeUsFormMessage
 
 const locationViewButton = document.querySelector(".contacts-map");
 const locationPopup = document.querySelector(".our-location");
+const locationPopupCloseButton = document.querySelector(".our-location-close");
 
 const sliderTabsMenu = document.querySelectorAll(".slider-controls");
-//const servicesSlides = document.querySelector(".services-list li");
 
 const userName = (localStorage && localStorage.getItem("userName")) ?
-	localStorage.getItem("userName") : null;
+    localStorage.getItem("userName") : null;
 
 const userEmail = (localStorage && localStorage.getItem("userEmail")) ?
-	localStorage.getItem("userEmail") : null;
+    localStorage.getItem("userEmail") : null;
 
 /* Выпадающая карта */
 
 locationViewButton.addEventListener("click", function (clickEvent) {
-	clickEvent.preventDefault();
-	overlay.classList.add("show");
-	locationPopup.classList.add("show");
-	locationPopup.classList.add("show-animation");
-	setTimeout(function () {
-		locationPopup.classList.remove("show-animation");
-	}, 500);
+    clickEvent.preventDefault();
+    overlay.classList.add("show");
+    locationPopup.classList.add("show");
+    locationPopup.classList.add("show-animation");
+    setTimeout(function () {
+        locationPopup.classList.remove("show-animation");
+    }, 500);
 });
 
 locationViewButton.addEventListener("keydown", function (clickEvent) {
-	if (clickEvent.keyCode === 27) closeLocationPopup();
+    if (clickEvent.keyCode === 27) closeLocationPopup();
+});
+
+locationPopupCloseButton.addEventListener("click", function (clickEvent) {
+    clickEvent.preventDefault();
+    closeLocationPopup();
 });
 
 const closeLocationPopup = function () {
-	overlay.classList.remove("show");
-	locationPopup.classList.remove("show");
+    overlay.classList.remove("show");
+    locationPopup.classList.remove("show");
 }
 
 /* Окончание кода выпадающей карты */
@@ -49,51 +54,51 @@ const closeLocationPopup = function () {
 /* Выпадающеее окно обратной связи */
 
 writeUsViewButton.addEventListener("click", function (clickEvent) {
-	clickEvent.preventDefault();
-	overlay.classList.add("show");
-	writeUsPopup.classList.add("show");
-	writeUsPopup.classList.add("show-animation");
-	setTimeout(function () {
-		writeUsPopup.classList.remove("show-animation");
-	}, 500);
+    clickEvent.preventDefault();
+    overlay.classList.add("show");
+    writeUsPopup.classList.add("show");
+    writeUsPopup.classList.add("show-animation");
+    setTimeout(function () {
+        writeUsPopup.classList.remove("show-animation");
+    }, 500);
 
-	if (userName && userEmail) {
-		writeUsFormName.value = userName;
-		writeUsFormEmail.value = userEmail;
-		writeUsFormMessage.focus();
-	} else writeUsFormName.focus();
+    if (userName && userEmail) {
+        writeUsFormName.value = userName;
+        writeUsFormEmail.value = userEmail;
+        writeUsFormMessage.focus();
+    } else writeUsFormName.focus();
 });
 
 writeUsForm.addEventListener("submit", function (submitEvent) {
-	if (!writeUsFormName.value || !writeUsFormEmail.value || !writeUsFormMessage.value) {
-		submitEvent.preventDefault();
-		writeUsPopup.classList.add("error");
-		setTimeout(function () {
-			writeUsPopup.classList.remove("error");
-		}, 800);
-		writeUsFormFields.forEach(function (item) {
-			if (!item.value) {
-				item.classList.add("incorrect");
-			}
-		})
-	} else if (localStorage) {
-		localStorage.setItem("userName", writeUsFormName.value);
-		localStorage.setItem("userEmail", writeUsFormEmail.value);
-	}
+    if (!writeUsFormName.value || !writeUsFormEmail.value || !writeUsFormMessage.value) {
+        submitEvent.preventDefault();
+        writeUsPopup.classList.add("error");
+        setTimeout(function () {
+            writeUsPopup.classList.remove("error");
+        }, 800);
+        writeUsFormFields.forEach(function (item) {
+            if (!item.value) {
+                item.classList.add("incorrect");
+            }
+        })
+    } else if (localStorage) {
+        localStorage.setItem("userName", writeUsFormName.value);
+        localStorage.setItem("userEmail", writeUsFormEmail.value);
+    }
 });
 
 writeUsPopupCloseButton.addEventListener("click", function (clickEvent) {
-	clickEvent.preventDefault();
-	closeWriteUsPopup();
+    clickEvent.preventDefault();
+    closeWriteUsPopup();
 });
 
 writeUsPopup.addEventListener("keydown", function (keypressEvent) {
-	if (keypressEvent.keyCode === 27) closeWriteUsPopup();
+    if (keypressEvent.keyCode === 27) closeWriteUsPopup();
 });
 
 const closeWriteUsPopup = function () {
-	overlay.classList.remove("show");
-	writeUsPopup.classList.remove("show");
+    overlay.classList.remove("show");
+    writeUsPopup.classList.remove("show");
 }
 
 /* Окончание кода выпадающей формы фидбека */
@@ -101,30 +106,30 @@ const closeWriteUsPopup = function () {
 /* Слайдеры */
 
 sliderTabsMenu.forEach(function (tabsMenuItem) {
-	const sliderTabsMenuControls = tabsMenuItem.querySelectorAll("button");
-	sliderTabsMenuControls.forEach(function (button) {
-		button.addEventListener("click", function (clickEvent) {
-			sliderTabsMenuControls.forEach(function (button, Buttonindex) {
-				button.classList.remove("active");
-				if (clickEvent.target === button) {
-					button.classList.add("active");
-					tabsMenuItem.nextElementSibling
-						.querySelectorAll(".slider-item")
-						.forEach(function (relativeSlide, slideIndex) {
-							relativeSlide.classList.remove("active");
-							if (Buttonindex === slideIndex) {
-								relativeSlide.classList.add("active");
-							}
-						});
-				}
-			});
-		});
-	});
+    const sliderTabsMenuControls = tabsMenuItem.querySelectorAll("button");
+    sliderTabsMenuControls.forEach(function (button) {
+        button.addEventListener("click", function (clickEvent) {
+            sliderTabsMenuControls.forEach(function (button, Buttonindex) {
+                button.classList.remove("active");
+                if (clickEvent.target === button) {
+                    button.classList.add("active");
+                    tabsMenuItem.nextElementSibling
+                        .querySelectorAll(".slider-item")
+                        .forEach(function (relativeSlide, slideIndex) {
+                            relativeSlide.classList.remove("active");
+                            if (Buttonindex === slideIndex) {
+                                relativeSlide.classList.add("active");
+                            }
+                        });
+                }
+            });
+        });
+    });
 });
 
 /* Окончание кода слайдеров */
 
 overlay.addEventListener("click", function () {
-	closeWriteUsPopup();
-	closeLocationPopup();
+    closeWriteUsPopup();
+    closeLocationPopup();
 });
